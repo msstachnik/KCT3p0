d6eea205ad669f86864bec03fbb333e30c17eb89
Mateusz Stachnik <msstachnik@gmail.com>
2015-06-03 01:35:29 +0200

    R8_1
    Deleted old files

--- differenction.m
+++ differenction.m
@@ -1,5 +1,5 @@
 function Y = differenction(X, T)
-% funkcja licz¹ca pochodn¹ z wektora wartosci i czasu
+% funkcja licz¹ca pochodn¹ z wektora wartosci i czasu 
 T = [T(1) T T(end)];
 X = [X(1) X X(end)];
 dx = X(3:end) - X(1:end-2);
